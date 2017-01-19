581d8b7dfa6fbde6bc6a94212679a9cc30544970
qiongz <qiongzhunow@gmail.com>
2017-01-19 22:30:25 +0800

    generating basis

--- main.cpp
+++ main.cpp
@@ -5,29 +5,43 @@
 #include<string>
 #include<unistd.h>
 #include<vector>
+#include<list>
+#include<map>
 #include<algorithm>
 //#include"mkl.h"
 
 using namespace std;
 
 int main(int argc,char *argv[]){
-    int n_site,n_spin;
+    int n_site,n_el;
+    int n_elu,n_eld;
+    int Sz_tot;
+    double t,U;
+
+    /* initialize parameters */
     n_site=2; 
-    n_spin=2;
-    
+    n_el=2;
+    t=1.0;
+    U=0.5;
     void usage(char*);
     int getopt(int argc,char * const argv[],const char *optstring);
     extern char *optarg;
     extern int optind, opterr, optopt;
     int ch,errFlag;
     errFlag=0;
-    while((ch=getopt(argc,argv,"l:n:h:"))!=-1){
+    while((ch=getopt(argc,argv,"l:n:t:U:h:"))!=-1){
     switch(ch){
         case 'l':
             n_site=atoi(optarg);
             break;
         case 'n':
-            n_spin=atoi(optarg);
+            n_el=atoi(optarg);
+            break;
+        case 't':
+            t=atof(optarg);
+            break;
+        case 'U':
+            U=atof(optarg);
             break;
         case 'h':
             errFlag++;
@@ -41,14 +55,22 @@ int main(int argc,char *argv[]){
         usage(argv[0]);
         exit(2);
     }
-    
 
+    /* generating basis */
+    int i,j,k,l;
+    for(n_elu=0;n_elu<=n_el;n_elu++){
+        Sz_tot=2*n_elu-n_el;
+        basis sector(n_site,n_elu,n_eld);
+    
+    }
     return 0;
 }
 void usage(char *target){
-    cout<<"Usage: "<<target<<" -[Options]\n";
+    cout<<"Usage: "<<target<<" [Options]\n";
     cout<<"Options:\n";
-    cout<<"   -l    Number of sites\n";
-    cout<<"   -n    Number of spins\n";
-    cout<<"Default: l=2  n=2"<<endl;
+    cout<<"  -l                       Number of sites\n";
+    cout<<"  -n                       Number of electrons\n";
+    cout<<"  -t                       Hopping strength\n";
+    cout<<"  -U                       Onsite replusion energy\n";
+    cout<<"Default: (l,n,t,U) = (2,2,1.0,0.5)"<<endl;
 }
