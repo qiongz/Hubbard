c60fdde7570e5f7d9be35c1bda5fbf2fd05c5e3f
qiongz <qiongzhunow@gmail.com>
2017-01-19 22:30:09 +0800

    generating basis

--- basis.h
+++ basis.h
@@ -1,18 +1,26 @@
 #include<iostream>
+#include<bitset>
+#include<iomanip>
+#include<string.h>
+#include<cstdlib>
 using namespace std;
 
 class basis{
 private:
-    int u_config,d_config;   // up/down-spin configurations
-    int u_id,d_id;           // up/down-spin identification numbers
-    int u_J,d_J,J;             // up/down-spin J values, total J value
+    int n_site,n_elu,n_eld;  // No. of sites, spin-up/down electrons
+    int nb_up,nb_down;        // No. of basis for spin-up/down electrons
+    map<int,int> elu,eld;     // basis set of spin-up/down electrons
 
 public:
     explicit basis();
-    basis(int,int);
+    basis(int,int,int);
     const basis & operator=(const basis &);
     ~basis();
     basis hopping(int,int);
     basis potential(int);
+    void generate_up(int);
+    void generate_down(int);
+    void factorial(int,int);
+    void print();
 };
     
