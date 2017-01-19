eb72a909ae073f1398bcc165d794683dac2ab5ea
qiongz <qiongzhunow@gmail.com>
2017-01-20 00:38:02 +0800

    basis library

--- basis.h
+++ basis.h
@@ -1,27 +1,32 @@
 #include<iostream>
-#include<bitset>
-#include<iomanip>
 #include<string.h>
-#include<cstdlib>
+#include<iomanip>
+#include<bitset>
 #include<map>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 class basis{
 private:
     int n_site,n_elu,n_eld;  // No. of sites, spin-up/down electrons
-    int nb_up,nb_down;        // No. of basis for spin-up/down electrons
-    map<int,int> elu,eld;     // basis set of spin-up/down electrons
+    map<int,int> elu,eld;     // basis set of spin-up/down electrons, I-J table
 
 public:
+    int n_basis;               // No. of basis
+    int nb_up,nb_down;        // No. of basis for spin-up/down electrons
+    vector<int> relu,reld;    // reversal table, J->I, Lin's Table is a 2D array 
     explicit basis();
     basis(int,int,int);
     const basis & operator=(const basis &);
     ~basis();
-    //basis hopping(int,int);
-    //basis potential(int);
+    int hopping_up(int,int);
+    int hopping_down(int,int);
+    int potential(int,int,int);
+    int factorial(int,int);
+    void init();
     void generate_up(int);
     void generate_down(int);
-    int factorial(int,int);
     void print();
 };
     
