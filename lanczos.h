0ce9f540e52e2d9c96fdad1462e359f7e9c11416
qiongz <qiongzhunow@gmail.com>
2017-01-20 14:01:41 +0800

    diagonalization

--- lanczos.h
+++ lanczos.h
@@ -1,2 +1,6 @@
+#ifndef LANCZOS_H
+#define LANCZOS_H
 extern "C" int dsyev_(char *, char *, int *, double *, int*, double *, double *, int *, int *);
 void diag(double *h, double *e, int l);
+
+#endif
