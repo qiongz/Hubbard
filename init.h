d576f27e52a241984ea3319912f112f8fac8b397
qiongz <qiongzhunow@gmail.com>
2017-01-20 14:02:16 +0800

    initalization

--- init.h
+++ init.h
@@ -1,4 +1,8 @@
+#ifndef INIT_H
+#define INIT_H
 #include<unistd.h>
 #include<iostream>
 void usage(char *target);
 void init(int *n_site, int *n_el, double *t, double *U, int argc,char *argv[]);
+
+#endif
