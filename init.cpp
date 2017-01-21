51f981fc043baf5eca546e77d452ad84fe70c356
qiongz <qiongzhunow@gmail.com>
2017-01-21 18:37:49 +0800

    initialization

--- init.cpp
+++ init.cpp
@@ -1,4 +1,3 @@
-#include<unistd.h>
 #include"init.h"
 void usage(char *target){
     std::cout<<"Usage: "<<target<<" [Options]\n";
@@ -9,19 +8,18 @@ void usage(char *target){
     std::cout<<"  -U                       Onsite replusion energy\n";
     std::cout<<"Default: (l,n,t,U) = (2,2,1.0,0.5)\n";
 }
-void init(int* n_site, int* n_el, double *t, double *U, int argc,char *argv[]){
-    int getopt(int argc,char * const argv[],const char *optstring);
+
+void init_argv(int* nsite, int* nel, double *t, double *U, int argc,char *argv[])
+{
     extern char *optarg;
-    extern int optind, opterr, optopt;
     int ch,errFlag;
-    errFlag=0;
     while((ch=getopt(argc,argv,"l:n:t:U:h:"))!=-1){
-    switch(ch){
+        switch(ch){
         case 'l':
-            *n_site=atoi(optarg);
+            *nsite=atoi(optarg);
             break;
         case 'n':
-            *n_el=atoi(optarg);
+            *nel=atoi(optarg);
             break;
         case 't':
             *t=atof(optarg);
@@ -36,9 +34,25 @@ void init(int* n_site, int* n_el, double *t, double *U, int argc,char *argv[]){
             errFlag++;
             break;
        }
-    }
-    if(errFlag) {
+     }
+     try{  
+     if(*nsite<=0)
+        throw std::logic_error("-l: positive value required !");
+     if(*nel<=0)
+        throw std::logic_error("-n: positive value required !");
+     if(*nsite<2*(*nel))
+        throw std::logic_error("-l -n: electron filling nu=(2*n/l)>1 !");
+     if(fabs(*t)<1e-8 && fabs(*U)<1e-8)
+        throw std::logic_error("-t -U: at least one finite coupling constant required !");
+     }catch(std::logic_error &e){
+        std::cout<<e.what()<<std::endl;
         usage(argv[0]);
         exit(2);
-    }
+     }
+    
+     if(errFlag){
+        usage(argv[0]);
+        exit(0);
+     }
 }
+
