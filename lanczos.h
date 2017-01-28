db2a020ff9804921df83eec884f2dec3ea10240a
qiongz <qiongzhunow@gmail.com>
2017-01-28 12:37:29 +0800

    lanczos basis

--- lanczos.h
+++ lanczos.h
@@ -1,25 +1,25 @@
 #ifndef LANCZOS_H
 #define LANCZOS_H
+#include"basis.h"
 class lbasis{
 public:
-      vector<double> coeff;
+      std::vector<double> coeff;
       int size;
       double norm;
       explicit lbasis();
-      lbasis(int n=0):size(n);
+      lbasis(const int n=0):size(n){};
       lbasis(const lbasis &);
       ~lbasis();
-      void init(int n):size(n);
+      void init(const int);
       double normalize();
-      lbasis &operator=(const lbasis &);
+      lbasis & operator=(const lbasis &);
       const lbasis operator+(const lbasis &);
       const lbasis operator*(const double &);
-      double &operator*(const lbasis &);
-      const lbasis hoperation=(const vector<double> &,const vector<int> &, const vector<int> &);
+      double operator*(const lbasis &);
+      lbasis hoperation(const std::vector<double> &,const std::vector<int> &, const std::vector<int> &);
 };
 
 extern "C" int dsyev_(char *, char *, int *, double *, int*, double *, double *, int *, int *);
 void diag(double *h, double *e, int l);
-void hoperation(double*,double*);
 
 #endif
