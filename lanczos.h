0dbd9954d599bf20737b78abd3493904f7e9d8c7
qiongz <qiongzhunow@gmail.com>
2017-01-28 12:59:53 +0800

    lanczos basis

--- lanczos.h
+++ lanczos.h
@@ -6,8 +6,8 @@ public:
       std::vector<double> coeff;
       int size;
       double norm;
-      explicit lbasis();
-      lbasis(const int n=0):size(n){};
+      lbasis();
+      lbasis(const int);
       lbasis(const lbasis &);
       ~lbasis();
       void init(const int);
@@ -16,7 +16,7 @@ public:
       const lbasis operator+(const lbasis &);
       const lbasis operator*(const double &);
       double operator*(const lbasis &);
-      lbasis hoperation(const std::vector<double> &,const std::vector<int> &, const std::vector<int> &);
+      lbasis hoperation(const vector<double> &,const vector<int> &, const vector<int> &);
 };
 
 extern "C" int dsyev_(char *, char *, int *, double *, int*, double *, double *, int *, int *);
